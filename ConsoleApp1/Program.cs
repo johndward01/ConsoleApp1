5677043a26ba6034b35c4462b73dfdda53204392
johndward01 <johndward01@gmail.com>
2022-09-06 14:47:59 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Console.Write("Please enter your first name: ");
+string firstName = Console.ReadLine();
+
+Console.Write("Please enter your last name: ");
+string lastName = Console.ReadLine();
+Console.WriteLine();
+//Console.WriteLine("Hello, " + firstName + " " + lastName + "!"); // concatenation
+//Console.WriteLine("Hello, {0} {1}!", firstName, lastName); // composite formatting
+Console.WriteLine($"Hello, {firstName} {lastName}!");
