18dd559d5cbd6e0d8c509219e5ae940ca7fab9d4
John Ward <johndward01@gmail.com>
2022-09-08 09:06:21 -0500

    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-
+﻿
 Console.Write("Please enter your first name: ");
 string firstName = Console.ReadLine();
 
